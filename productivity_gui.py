740566bf9d2d5605cf68dcd09b1464dbb3b7d9de
ChrisMCodes <34985965+ChrisMCodes@users.noreply.github.com>
2020-08-13 16:57:11 -0700

    Now fully functional!

--- productivity_gui.py
+++ productivity_gui.py
@@ -10,6 +10,9 @@
 # so that it can be used as a tasklist later
 
 from tkinter import *
+import sys
+import time
+import playsound
 
 class Task():
     def __init__(self, name, time):
@@ -52,15 +55,59 @@ def a_click():
     label = Label(root, text=name + " added to your tasklist")
     label.pack()
 
+def countdown_pop_up(mins, taskname):
+    '''creates gui for countdown'''
+    root = Tk()
+    root.title = ("Countdown")
+    label = Label(root, 
+                     fg = "green", 
+                     font = "Arial 32 bold")
+    label.pack()
+    button = Button(root, 
+                       text = "Stop", 
+                       width = 25, 
+                       command = root.destroy)
+    button.pack()
+    countdown(root, label, mins, taskname)
+    root.destroy()
+    root.mainloop()
+    pass
 
-
+def countdown(root, label, mins, taskname):
+    '''countdown for pop-up'''
+    i = mins * 60
+    while i >= 0:
+        minutes = i / 60
+        seconds = round(i % 60, 0)
+        counter = "%d:%02d" % (minutes, seconds)
+        label.config(text = counter + "\n" + taskname)
+        root.update()
+        time.sleep(1)
+        i -= 1
+        if i == 0:
+            label.config(text = "0:00 \nTime is up!")
+            root.update()
+            playsound.playsound("alarm.mp3")
 
     
+# Pop up to get user info
+task_button = Button(root, text="Enter your task", command=a_click)
+task_button.pack()
 
-button = Button(root, text="Enter your task", command=a_click)
-button.pack()
+exit_button = Button(root, text="Click here when finished", command=root.destroy)
+exit_button.pack()
 
 root.mainloop()
 
+
+#Counter!
 for task in tasklist:
-    print("{} for {} mins".format(task.get_name(), task.get_time()))
+    try:
+        t = task.get_time()
+        t = float(t)
+        n = task.get_name()
+        countdown_pop_up(t, n)
+    except:
+        print("Please enter valid numerical values for your times on task.")
+        print("Program exiting")
+        sys.exit(1)
